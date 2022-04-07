c8e0819eb76d5df0a9c28f7062d9893693ba4d0f
Teo <teo.kkim@kakaocorp.com>
2022-04-08 01:18:14 +0900

    Fix

--- src/main/java/com/example/demo/Main.java
+++ src/main/java/com/example/demo/Main.java
@@ -1,12 +1,25 @@
 package com.example.demo;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import java.util.Map;
 
-@Controller
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class Main {
-	@GetMapping("/home")
-	public String api() {
-		return "Home";
+	@GetMapping("/api")
+	public String api(@RequestHeader Map<String, Object> header,  HttpServletRequest request) {
+		StringBuilder sb = new StringBuilder();
+		sb.append(request.getMethod()).append(" ");
+		sb.append(request.getProtocol()).append(" ");
+		sb.append(request.getRequestURI()).append(System.getProperty("line.separator"));
+		for (Map.Entry<String, Object> entry : header.entrySet()) {
+			sb.append(entry.getKey()).append(": ").append(entry.getValue()).append("\n");
+		}
+		System.out.println(sb.toString());
+		return sb.toString();
 	}
 }
